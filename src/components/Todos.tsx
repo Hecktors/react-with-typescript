10cb356304629805dd25e230ce2f114f5b3348bd
Torsten Heckmann <torstenheckmann@hectorair.localdomain>
2022-05-15 12:31:04 +0200

    Add Todos component

---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <ul>
+      <li>Learn Typescript</li>
+      <li>Learn Unit Tests</li>
+    </ul>
+  )
+}
+
+export default Todos
